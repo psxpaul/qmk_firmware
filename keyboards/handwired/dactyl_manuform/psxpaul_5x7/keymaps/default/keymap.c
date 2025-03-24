f24b8455126694e472c3c8afe2c0f817dfb060eb
Paul Roberts <psxpaul@gmail.com>
2025-03-23 18:14:19 -0700

    fix 5x7 keyboard and try out home row mods and num layer, PLUS sticky layer mods

--- keyboards/handwired/dactyl_manuform/psxpaul_5x7/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/psxpaul_5x7/keymaps/default/keymap.c
@@ -6,31 +6,46 @@
 // entirely and just use numbers.
 #define _QWERTY 0
 #define _FN     1
+#define _NUM    2
 
-// Some basic macros
-#define TASK   LCTL(LSFT(KC_ESC))
-#define TAB_R  LCTL(KC_TAB)
-#define TAB_L  LCTL(LSFT(KC_TAB))
-#define TAB_RO LCTL(LSFT(KC_T))
+// List of modifier keys in the same order as in the `mod_state` array
+const uint16_t mod_keys[] = {
+    KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI,  // Left modifiers
+    KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI   // Right modifiers
+};
+
+// Calculate the number of modifiers using sizeof
+#define NUM_MODIFIERS (sizeof(mod_keys) / sizeof(mod_keys[0]))
+
+// Track the state of modifiers (left and right modifiers)
+bool mod_state[NUM_MODIFIERS] = {false};  // Array to track the state of all modifiers
+
+// Layer state management
+layer_state_t layer_state_set_user(layer_state_t state) {
+    if (get_highest_layer(state) == _QWERTY) {
+        unregister_mods(MOD_MASK_CTRL | MOD_MASK_SHIFT | MOD_MASK_ALT | MOD_MASK_GUI);
+    }
+    return state;
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x7(
         // left hand
         _______,    KC_GRV,     KC_1,    KC_2,    KC_3,   KC_4,   KC_5,
         _______,    KC_TAB,     KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,
-        _______,    KC_ESC,     KC_A,    KC_S,    KC_D,   KC_F,   KC_G,
+        _______,    KC_ESC,     MT(MOD_LCTL, KC_A),    MT(MOD_LALT, KC_S),    MT(MOD_LGUI, KC_D),   MT(MOD_LSFT, KC_F),   KC_G,
         _______,    KC_LSFT,    KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
                                                   _______, _______,
-                                                                 KC_BSPC, KC_DEL,
+                                                                 KC_BSPC, LT(_NUM, KC_DEL),
                                                                           KC_LCTL, KC_LALT,
                                                                           KC_LGUI, MO(_FN),
         // right hand
                       KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS,    _______,
                       KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,    _______,
-                      KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,    _______,
+                      KC_H,    MT(MOD_RSFT, KC_J),    MT(MOD_RGUI, KC_K),     MT(MOD_RALT, KC_L),     MT(MOD_RCTL, KC_SCLN),  KC_QUOT,    _______,
                       KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,    _______,
                                _______, _______,
-                 KC_ENT, KC_SPC,
+                 LT(_NUM, KC_ENT), KC_SPC,
         KC_RALT, KC_RCTL,
         MO(_FN),  KC_RGUI
     ),
@@ -55,4 +70,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          _______, _______,
          _______, _______
     ),
+
+    [_NUM] = LAYOUT_5x7(
+        // left hand
+        _______,    _______,     _______,    _______,    _______,   _______,   _______,
+        _______,    _______,     _______,       KC_7,       KC_8,      KC_9,   _______,
+        _______,    _______,     _______,       KC_4,       KC_5,      KC_6,   _______,
+        _______,    _______,     _______,       KC_1,       KC_2,      KC_3,   _______,
+                                             _______, _______,
+                                                                    KC_0, _______,
+                                                                          _______, _______,
+                                                                          _______, _______,
+        // right hand
+                      _______,    _______,    _______,     _______,     _______,     _______,    _______,
+                      _______,       KC_7,       KC_8,        KC_9,     _______,     _______,    _______,
+                      _______,       KC_4,       KC_5,        KC_6,     _______,     _______,    _______,
+                      _______,       KC_1,       KC_2,        KC_3,     _______,     _______,    _______,
+                               _______, _______,
+                 _______,    KC_0,
+        _______, _______,
+        _______, _______
+    ),
 };
+
+// Detect when modifiers are active
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    for (int i = 0; i < NUM_MODIFIERS; i++) {
+        if (keycode == mod_keys[i]) {
+            if (record->event.pressed) {
+                mod_state[i] = true;  // Set modifier state to active
+                return true;
+            } else if (IS_LAYER_ON(_QWERTY)) {
+                mod_state[i] = false; // Set modifier state to inactive
+                return true;
+            } else {
+                return false;   // do not process the keyup on the layer
+            }
+        }
+    }
+    return true;  // Default behavior for other keys
+}
