29f1a7aa8438fb0a6fca24f1d029c31628a1d7f7
Paul Roberts <psxpaul@gmail.com>
2025-03-25 11:39:50 -0700

    re-organize things a little, and add an FN layer

--- keyboards/handwired/dactyl_manuform/psxpaul_5x7/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/psxpaul_5x7/keymaps/default/keymap.c
@@ -5,8 +5,21 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _FN     1
+#define _NAV    1
 #define _NUM    2
+#define _FN     3
+
+// Left-hand home row mods
+#define HOME_A LCTL_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RGUI_T(KC_K)
+#define HOME_L RALT_T(KC_L)
+#define HOME_SC RCTL_T(KC_SCLN)
 
 // List of modifier keys in the same order as in the `mod_state` array
 const uint16_t mod_keys[] = {
@@ -30,66 +43,47 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x7(
-        // left hand
-        _______,    KC_GRV,     KC_1,    KC_2,    KC_3,   KC_4,   KC_5,
-        _______,    KC_TAB,     KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,
-        _______,    KC_ESC,     MT(MOD_LCTL, KC_A),    MT(MOD_LALT, KC_S),    MT(MOD_LGUI, KC_D),   MT(MOD_LSFT, KC_F),   KC_G,
-        _______,    KC_LSFT,    KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-                                                  _______, _______,
-                                                                 KC_BSPC, LT(_NUM, KC_DEL),
-                                                                          KC_LCTL, KC_LALT,
-                                                                          KC_LGUI, MO(_FN),
-        // right hand
-                      KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS,    _______,
-                      KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,    _______,
-                      KC_H,    MT(MOD_RSFT, KC_J),    MT(MOD_RGUI, KC_K),     MT(MOD_RALT, KC_L),     MT(MOD_RCTL, KC_SCLN),  KC_QUOT,    _______,
-                      KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,    _______,
-                               _______, _______,
-                 LT(_NUM, KC_ENT), KC_SPC,
-        KC_RALT, KC_RCTL,
-        MO(_FN),  KC_RGUI
+        _______,    KC_GRV,     KC_1,    KC_2,    KC_3,   KC_4,   KC_5,                                             KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS,    _______,
+        _______,    KC_TAB,     KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,                                             KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,    _______,
+        _______,    KC_ESC,     HOME_A,  HOME_S,  HOME_D, HOME_F, KC_G,                                             KC_H,    HOME_J,  HOME_K,   HOME_L,   HOME_SC,  KC_QUOT,    _______,
+        _______,    KC_LSFT,    KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,                                             KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,    _______,
+                                                  _______, _______,                                                          _______, _______,
+                                                        LT(_FN, KC_BSPC), LT(_NUM, KC_DEL),         LT(_NUM, KC_ENT), LT(_FN,KC_SPC),
+                                                                          KC_LCTL, KC_LALT,         KC_RALT, KC_RCTL,
+                                                                          KC_LGUI, MO(_NAV),         MO(_NAV),  KC_RGUI
     ),
 
-    [_FN] = LAYOUT_5x7(
-        // left hand
-        _______,   _______,   KC_F1,     KC_F2,      KC_F3,    KC_F4,     KC_F5,
-        _______,   _______,   _______,   _______,    KC_UP,    _______,   KC_HOME,
-        _______,   _______,   _______,   KC_LEFT,    KC_DOWN,  KC_RGHT,   KC_SPC,
-        _______,   _______,   _______,   KC_VOLD,   KC_MUTE,   KC_VOLU,   KC_END,
-                                         _______,   _______,
-                                                               _______, _______,
-                                                                        _______, _______,
-                                                                        _______, _______,
-        // right hand
-                       KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    _______,
-                       KC_PGUP,   KC_LBRC,   KC_LCBR,   KC_RCBR,   KC_RBRC,   KC_F12,    _______,
-                       KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   _______,   KC_EQL,    _______,
-                       KC_PGDN,   _______,   _______,   _______,   _______,   _______,   _______,
-                                             _______,   _______,
-              _______, _______,
-         _______, _______,
-         _______, _______
+    [_NAV] = LAYOUT_5x7(
+        _______,   _______,   KC_F1,     KC_F2,      KC_F3,    KC_F4,     KC_F5,                                    KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    _______,
+        _______,   _______,   _______,   _______,    KC_UP,    _______,   KC_HOME,                                  KC_PGUP,   KC_LBRC,   KC_LCBR,   KC_RCBR,   KC_RBRC,   KC_F12,    _______,
+        _______,   _______,   _______,   KC_LEFT,    KC_DOWN,  KC_RGHT,   KC_SPC,                                   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   _______,   KC_EQL,    _______,
+        _______,   _______,   _______,   KC_VOLD,   KC_MUTE,   KC_VOLU,   KC_END,                                   KC_PGDN,   _______,   _______,   _______,   _______,   _______,   _______,
+                                         _______,   _______,                                                                   _______,   _______,
+                                                               _______, _______,                             _______, _______,
+                                                                        _______, _______,           _______, _______,
+                                                                        _______, _______,           _______, _______
     ),
 
     [_NUM] = LAYOUT_5x7(
-        // left hand
-        _______,    _______,     _______,    _______,    _______,   _______,   _______,
-        _______,    _______,     _______,       KC_7,       KC_8,      KC_9,   _______,
-        _______,    _______,     _______,       KC_4,       KC_5,      KC_6,   _______,
-        _______,    _______,     _______,       KC_1,       KC_2,      KC_3,   _______,
-                                             _______, _______,
-                                                                    KC_0, _______,
-                                                                          _______, _______,
-                                                                          _______, _______,
-        // right hand
-                      _______,    _______,    _______,     _______,     _______,     _______,    _______,
-                      _______,       KC_7,       KC_8,        KC_9,     _______,     _______,    _______,
-                      _______,       KC_4,       KC_5,        KC_6,     _______,     _______,    _______,
-                      _______,       KC_1,       KC_2,        KC_3,     _______,     _______,    _______,
-                               _______, _______,
-                 _______,    KC_0,
-        _______, _______,
-        _______, _______
+        _______,    _______,     _______,    _______,    _______,   _______,   _______,                             _______,    _______,    _______,     _______,     _______,     _______,    _______,
+        _______,    _______,     _______,       KC_7,       KC_8,      KC_9,   _______,                             _______,       KC_7,       KC_8,        KC_9,     _______,     _______,    _______,
+        _______,    _______,     _______,       KC_4,       KC_5,      KC_6,   _______,                             _______,       KC_4,       KC_5,        KC_6,     _______,     _______,    _______,
+        _______,    _______,     _______,       KC_1,       KC_2,      KC_3,   _______,                             _______,       KC_1,       KC_2,        KC_3,     _______,     _______,    _______,
+                                             _______, _______,                                                                  _______, _______,
+                                                                    KC_0, _______,                           _______,    KC_0,
+                                                                          _______, _______,         _______, _______,
+                                                                          _______, _______,         _______, _______
+    ),
+
+    [_FN] = LAYOUT_5x7(
+        _______,    _______,     _______,    _______,    _______,   _______,   _______,                             _______,    _______,    _______,     _______,     _______,     _______,    _______,
+        _______,    _______,      KC_F12,      KC_F7,      KC_F8,     KC_F9,   _______,                             _______,      KC_F7,      KC_F8,       KC_F9,      KC_F12,     _______,    _______,
+        _______,    _______,      KC_F11,      KC_F4,      KC_F5,     KC_F6,   _______,                             _______,      KC_F4,      KC_F5,       KC_F6,      KC_F11,     _______,    _______,
+        _______,    _______,      KC_F10,      KC_F1,      KC_F2,     KC_F3,   _______,                             _______,      KC_F1,      KC_F2,       KC_F3,      KC_F10,     _______,    _______,
+                                             _______, _______,                                                                  _______, _______,
+                                                                 _______, _______,                           _______, _______,
+                                                                          _______, _______,         _______, _______,
+                                                                          _______, _______,         _______, _______
     ),
 };
 
