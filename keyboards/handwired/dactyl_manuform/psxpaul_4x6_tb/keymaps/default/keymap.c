ac2d7eeec7297b19369d90837f2704e80c6c06c9
Paul Roberts <psxpaul@gmail.com>
2025-06-06 11:45:48 -0700

    adding new 4x6 dactyl build with trackball

---
+++ keyboards/handwired/dactyl_manuform/psxpaul_4x6_tb/keymaps/default/keymap.c
@@ -0,0 +1,55 @@
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _QWERTY 0
+#define _NAV    1
+#define _SYM    2
+#define _NUM    3
+
+// QWERTY HRMs
+#define HOME_A LCTL_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RGUI_T(KC_K)
+#define HOME_L RALT_T(KC_L)
+#define HOME_SC RCTL_T(KC_SCLN)
+
+// Thumb keys
+#define THUMB_L1 LT(_SYM, KC_BSPC)
+#define THUMB_L2 LT(_NUM, KC_DEL)
+#define THUMB_L3 LT(_NAV, KC_ESC)
+#define THUMB_R3 LT(_NAV, KC_TAB)
+#define THUMB_R2 LT(_NUM, KC_ENT)
+#define THUMB_R1 LT(_SYM, KC_SPC)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWERTY] = LAYOUT_4x6(
+        KC_TAB,   KC_Q,          KC_W,             KC_E,             KC_R,             KC_T,                     KC_Y,     KC_U,             KC_I,           KC_O,             KC_P,             KC_BSLS,
+        KC_ESC,   HOME_A,        HOME_S,           HOME_D,           HOME_F,           KC_G,                     KC_H,     HOME_J,           HOME_K,         HOME_L,           HOME_SC,          KC_QUOT,
+        KC_LSFT,  KC_Z,          KC_X,             KC_C,             KC_V,             KC_B,                     KC_N,     KC_M,             KC_COMM,        KC_DOT,           KC_SLSH,          KC_RSFT,
+                                                   THUMB_L1,         THUMB_L2,         THUMB_L3,                 THUMB_R3, THUMB_R2,         THUMB_R1
+    ),
+    [_SYM] = LAYOUT_4x6(
+        KC_GRV,   KC_EXLM,       KC_AT,            KC_HASH,          KC_DLR,           KC_PERC,                  KC_CIRC,  KC_AMPR,          KC_ASTR,        KC_LPRN,          KC_RPRN,          KC_MINS,
+        KC_TILD,  LCTL_T(KC_1),  LALT_T(KC_2),     LGUI_T(KC_3),     LSFT_T(KC_4),     KC_5,                     KC_6,     RSFT_T(KC_7),     RGUI_T(KC_8),   RALT_T(KC_9),     RCTL_T(KC_0),     KC_EQL,
+        KC_F11,   KC_F1,         KC_F2,            KC_F3,            KC_F4,            KC_F5,                    KC_F6,    KC_F7,            KC_F8,          KC_F9,            KC_F10,           KC_F12,
+                                                   _______,          _______,          _______,                  _______,  _______,          _______
+    ),
+    [_NUM] = LAYOUT_4x6(
+      G(KC_GRV),  XXXXXXX,       KC_7,             KC_8,             KC_9,             KC_MINS,                  XXXXXXX,  KC_7,             KC_8,           KC_9,             KC_PMNS,          KC_MINS,
+        XXXXXXX,  XXXXXXX,       LALT_T(KC_4),     LGUI_T(KC_5),     LSFT_T(KC_6),     KC_PLUS,                  XXXXXXX,  RSFT_T(KC_4),     RGUI_T(KC_5),   RALT_T(KC_6),     RCTL_T(KC_PPLS),  KC_PLUS,
+        XXXXXXX,     KC_0,       KC_1,             KC_2,             KC_3,             KC_EQL,                   KC_0,     KC_1,             KC_2,           KC_3,             KC_EQL,           KC_EQL,
+                                                   _______,          _______,          _______,                  _______,  _______,          _______
+    ),
+    [_NAV] = LAYOUT_4x6(
+        XXXXXXX,  KC_PGUP,       XXXXXXX,          KC_UP,            XXXXXXX,          KC_HOME,                  KC_PGUP,  KC_LBRC,          KC_LCBR,        KC_RCBR,          KC_RBRC,          XXXXXXX,
+        XXXXXXX,  XXXXXXX,       LALT_T(KC_LEFT),  LGUI_T(KC_DOWN),  LSFT_T(KC_RGHT),  KC_SPC,                   KC_LEFT,  RSFT_T(KC_DOWN),  RGUI_T(KC_UP),  RALT_T(KC_RGHT),  XXXXXXX,          XXXXXXX,
+        XXXXXXX,  KC_PGDN,       KC_VOLD,          KC_MUTE,          KC_VOLU,          KC_END,                   KC_PGDN,  KC_LPRN,          KC_LT,          KC_GT,            KC_RPRN,          XXXXXXX,
+                                                   _______,          _______,          _______,                  _______,  _______,          _______
+    ),
+};
---
+++ keyboards/handwired/dactyl_manuform/psxpaul_4x6_tb/readme.md
@@ -0,0 +1,89 @@
+## RP2040 Schematic
+![rp2040](rp2040_schematic.jpg)
+[link](https://www.aliexpress.us/item/3256804258715020.html?spm=a2g0o.order_list.order_list_main.21.11781802YYJvoL&gatewayAdapt=glo2usa)
+
+## PMW3389 Schematic
+![pmw3389](pmw3389_schematic.jpg)
+[link](https://www.tindie.com/products/citizenjoe/pmw3389-motion-sensor/)
+
+Pinout:
+```
+RST = Reset
+GND = Ground
+MT = Motion (active low interrupt line)
+SS = Slave Select / Chip Select
+SCK = SPI Clock
+MOSI = MOSI
+MISO = MISO
+VIN = Voltage in up to +5.5V
+```
+
+## Connections
+PMW3389 to RP2040
+```
+GND <=> GND
+MT <=> GP6
+SS <=> GP5 (SPI0 CSn)
+SCK <=> GP2 (SPI0 SCK)
+MOSI <=> GP3 (SPI0 TX)
+MISO <=> GP4 (SPI0 RX)
+VIN <=> 3v3
+```
+
+Matrix to RP2040
+```
+ROW1 <=> GP12
+ROW2 <=> GP13
+ROW3 <=> GP14
+ROW4 <=> GP15
+
+COL1 <=> GP21
+COL2 <=> GP20
+COL3 <=> GP19
+COL4 <=> GP18
+COL5 <=> GP17
+COL6 <=> GP16
+```
+
+TRRS to RP2040
+```
+GP0
+GP1
+VOUT (5V)
+GND
+```
+
+## Notes to Self
+Install QMK (on arch):
+
+    sudo pacman -S qmk
+    make git-submodule
+
+One time flashes to set the half in the eeprom (should not have to be done again):
+
+    qmk flash -kb handwired/dactyl_manuform/psxpaul_4x6_tb -km default -bl uf2-split-left
+    qmk flash -kb handwired/dactyl_manuform/psxpaul_4x6_tb -km default -bl uf2-split-right
+
+If you get "Chip already blank, to force erase use --force." error:
+
+    dfu-programmer atmega32u4 erase --force
+    (see https://github.com/qmk/qmk_firmware/issues/22472 or https://github.com/qmk/qmk_firmware/issues/22050#issuecomment-1722308968)
+
+To compile:
+
+    qmk compile -kb handwired/dactyl_manuform/psxpaul_4x6_tb -km default
+
+To flash:
+
+    qmk flash -kb handwired/dactyl_manuform/psxpaul_4x6_tb -km default
+
+   *important: do not use a hub/dock while flashing!*
+
+
+## Bootloader
+
+Enter the bootloader in 3 ways:
+
+* **Bootmagic reset**: Hold down the key at (0,0) in the matrix (usually the top left key or Escape) and plug in the keyboard
+* **Physical reset button**: Hold the boot button the PCB and press the rst button
+* **Keycode in layout**: Press the key mapped to `QK_BOOT` if it is available
