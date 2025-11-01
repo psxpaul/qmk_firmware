54e2717ae1d07de0ae188c0fc5a030d9b6fc51c3
Paul Roberts <psxpaul@gmail.com>
2025-11-01 15:20:59 -0700

    update 4x6tb build with working cursor config

--- keyboards/handwired/dactyl_manuform/psxpaul_4x6_tb/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/psxpaul_4x6_tb/keymaps/default/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "print.h"
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -8,6 +9,7 @@
 #define _NAV    1
 #define _SYM    2
 #define _NUM    3
+#define _MOUSE  4
 
 // QWERTY HRMs
 #define HOME_A LCTL_T(KC_A)
@@ -52,4 +54,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,  KC_PGDN,       KC_VOLD,          KC_MUTE,          KC_VOLU,          KC_END,                   KC_PGDN,  KC_LPRN,          KC_LT,          KC_GT,            KC_RPRN,          XXXXXXX,
                                                    _______,          _______,          _______,                  _______,  _______,          _______
     ),
+    [_MOUSE] = LAYOUT_4x6(
+        XXXXXXX,  XXXXXXX,       XXXXXXX,          XXXXXXX,          XXXXXXX,          XXXXXXX,                  XXXXXXX,  XXXXXXX,          XXXXXXX,        XXXXXXX,          XXXXXXX,          XXXXXXX,
+        XXXXXXX,  XXXXXXX,       MS_BTN1,          MS_BTN2,          MS_BTN3,          XXXXXXX,                  XXXXXXX,  MS_BTN1,          MS_BTN2,        MS_BTN3,          XXXXXXX,          XXXXXXX,
+        XXXXXXX,  XXXXXXX,       XXXXXXX,          XXXXXXX,          XXXXXXX,          XXXXXXX,                  XXXXXXX,  XXXXXXX,          XXXXXXX,        XXXXXXX,          XXXXXXX,          XXXXXXX,
+                                                   _______,          _______,          _______,                  _______,  _______,          _______
+    )
 };
+
+void keyboard_post_init_user(void) {
+    // debug_enable = true;
+
+    pointing_device_set_cpi_on_side(true, 100); //Set cpi on left side to a low value for slower scrolling.
+    pointing_device_set_cpi_on_side(false, 1500); //Set cpi on right side to a reasonable value for mousing.
+    set_auto_mouse_layer(_MOUSE);
+    set_auto_mouse_enable(true);
+}
+
+report_mouse_t pointing_device_task_combined_user(report_mouse_t left_report, report_mouse_t right_report) {
+    left_report.h = left_report.x;
+    left_report.v = -1 * left_report.y;
+    left_report.x = 0;
+    left_report.y = 0;
+
+    dprintf("Combining mouse reports(h: %d|%d, v: %d|%d)", left_report.h, right_report.h, left_report.v, right_report.v);
+
+    return pointing_device_combine_reports(left_report, right_report);
+}
--- keyboards/handwired/dactyl_manuform/psxpaul_4x6_tb/readme.md
+++ keyboards/handwired/dactyl_manuform/psxpaul_4x6_tb/readme.md
@@ -85,5 +85,5 @@ To flash:
 Enter the bootloader in 3 ways:
 
 * **Bootmagic reset**: Hold down the key at (0,0) in the matrix (usually the top left key or Escape) and plug in the keyboard
-* **Physical reset button**: Hold the boot button the PCB and press the rst button
+* **Physical reset button**: Hold the boot button when plugging in the device
 * **Keycode in layout**: Press the key mapped to `QK_BOOT` if it is available
